9d8575af38724069f72bed1815b3e11f1091cb1a
moreniekmeijer <niekjmeijer@gmail.com>
2025-01-30 22:58:20 +0100

    Started exercises part 2

--- src/constants/inventory.js
+++ src/constants/inventory.js
@@ -328,4 +328,29 @@ export let inventory = [
     },
 ];
 
+// opdracht 1a:
+const tvTypes = inventory.map((tv) => tv.type);
+console.log(tvTypes);
 
+// opdracht 1b:
+const soldTvs = inventory.filter((tv) => (tv.originalStock - tv.sold) <= 0);
+console.log(soldTvs);
+
+// opdracht 1c:
+const specificTv = inventory.find((tv) => tv.type === "NH3216SMART");
+console.log(specificTv);
+
+// opdracht 1d:
+const suitableForSport = inventory.map((tv) => {
+    const suitable = tv.refreshRate >= 100;
+    return "name: " + tv.brand + " " + tv.name + ", suitable: " + suitable;
+});
+console.log(suitableForSport);
+
+// // opdracht 1e:
+// const largeTvs = inventory.filter((tv) => (tv.availableSizes.some((size) => size >= 65)));
+// console.log(largeTvs);
+//
+// // opdracht 1f:
+// const ambilightTvs = inventory.filter((tv) => (tv.options.some((option) => (option.name === "ambiLight" && option.applicable))));
+// console.log("oplossing: " + ambilightTvs);
