f29d45c7330016002a9f17e8a9aaeaf52d4bd198
moreniekmeijer <niekjmeijer@gmail.com>
2025-02-01 22:36:00 +0100

    Finished exercise 3 and the bonus parts

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,12 @@ import soldTVs from "./helpers/soldTVs.js";
 import purchasedTVs from "./helpers/purchasedTVs.js";
 import toSellTVs from "./helpers/toSellTVs.js";
 import getFormattedNameTV from "./helpers/getFormattedNameTV.js";
-import {inventory, bestSellingTv} from "./constants/inventory.js";
+import {inventory, bestSellingTv, bestSellingTv as availableSizes} from "./constants/inventory.js";
 import getFormattedPriceTV from "./helpers/getFormattedPriceTV.js";
 import getFormattedSizesTV from "./helpers/getFormattedSizesTV.js";
 import check from "./assets/check.png";
 import minus from "./assets/minus.png";
+import outOfStock from "./assets/out-of-stock.png";
 
 console.log(soldTVs());
 console.log(purchasedTVs());
@@ -17,16 +18,32 @@ console.log(getFormattedPriceTV(bestSellingTv));
 console.log(getFormattedSizesTV(bestSellingTv));
 
 function App() {
-    function mostSoldFirst() {
+
+    // opdracht 3:
+    function sortBySold() {
+        inventory.sort((a, b) => b.sold - a.sold);
         console.log("Meest verkocht eerst");
+        console.log(inventory);
     }
 
-    function cheapestFirst() {
+    function sortByPrice() {
+        inventory.sort((a, b) => a.price - b.price);
         console.log("Goedkoopste eerst");
+        console.log(inventory);
     }
 
-    function bestSportFirst() {
+    function sortForSport() {
+        inventory.sort((a, b) => a.refreshRate - b.refreshRate);
         console.log("Meest geschikt voor sport eerst");
+        console.log(inventory);
+    }
+
+    // bonusopdracht 1:
+    function sortByLargestScreen() {
+        inventory.sort((a, b) => b.availableSizes[(availableSizes.availableSizes.length - 1)] -
+            a.availableSizes[(availableSizes.availableSizes.length - 1)]);
+        console.log("Grootste schermgroottes eerst");
+        console.log(inventory);
     }
 
     return (
@@ -68,28 +85,39 @@ function App() {
                 </div>
             </section>
             <section>
+                <h2>Alle merken</h2>
+                <ul>
+                    {inventory.map((tv) => <li key={tv.type}>{tv.brand}</li>)}
+                </ul>
                 <h2>{`Alle TV's`}</h2>
-                <button type="button" onClick={mostSoldFirst}>Meest verkocht eerst</button>
-                <button type="button" onClick={cheapestFirst}>Goedkoopste eerst</button>
-                <button type="button" onClick={bestSportFirst}>Meest geschikt voor sport eerst</button>
-                <div className="all-products">
-                    {inventory.map((tv) => {
-                        return <div key={tv.type} className="product">
-                            <span><img src={tv.sourceImg} alt="Samsung UHD 55AU7040 - Crystal"/></span>
-                            <div className="product-info">
-                                <p>{getFormattedNameTV(tv)}</p>
-                                <p>{getFormattedPriceTV(tv)}</p>
-                                <p>{getFormattedSizesTV(tv)}</p>
-                                <ul>
-                                    {tv.options.map((option) => { if (option.applicable === true) {
-                                        return <li key={option.name}><img src={check} alt="check"/> {option.name} </li> } else {
-                                        return <li key={option.name}><img src={minus} alt="unchecked"/> {option.name} </li> }
-                                    })}
-                                </ul>
-                            </div>
+                <button type="button" onClick={sortBySold}>Meest verkocht eerst</button>
+                <button type="button" onClick={sortByPrice}>Goedkoopste eerst</button>
+                <button type="button" onClick={sortForSport}>Meest geschikt voor sport eerst</button>
+                <button type="button" onClick={sortByLargestScreen}>Grootste schermgroottes eerst</button>
+                {inventory.map((tv) => {
+                    return <div key={tv.type} className="product">
+                        {/*bonusopdracht 2*/}
+                        {tv.originalStock - tv.sold === 0 &&
+                            <span className="sold-out"><img src={outOfStock} alt="Out of stock"/></span>}
+                        <span><img src={tv.sourceImg} alt="Samsung UHD 55AU7040 - Crystal"/></span>
+                        <div className="product-info">
+                            <p>{getFormattedNameTV(tv)}</p>
+                            <p>{getFormattedPriceTV(tv)}</p>
+                            <p>{getFormattedSizesTV(tv)}</p>
+                            <ul>
+                                {tv.options.map((option) => {
+                                    if (option.applicable === true) {
+                                        return <li key={option.name}><img src={check} alt="check"/> {option.name}
+                                        </li>
+                                    } else {
+                                        return <li key={option.name}><img src={minus} alt="unchecked"/> {option.name}
+                                        </li>
+                                    }
+                                })}
+                            </ul>
                         </div>
-                    })}
-                </div>
+                    </div>
+                })}
             </section>
         </>
     )
--- src/constants/inventory.js
+++ src/constants/inventory.js
@@ -328,33 +328,3 @@ export let inventory = [
     },
 ];
 
-// opdracht 1a:
-const tvTypes = inventory.map((tv) => tv.type);
-console.log(tvTypes);
-
-// opdracht 1b:
-const soldTvs = inventory.filter((tv) => (tv.originalStock - tv.sold) <= 0);
-console.log(soldTvs);
-
-// opdracht 1c:
-const specificTv = inventory.find((tv) => tv.type === "NH3216SMART");
-console.log(specificTv);
-
-// opdracht 1d:
-const suitableForSport = inventory.map((tv) => {
-    const suitable = tv.refreshRate >= 100;
-    return "name: " + tv.brand + " " + tv.name + ", suitable: " + suitable;
-});
-console.log(suitableForSport);
-
-// opdracht 1e:
-const largeTvs = inventory.filter((tv) => (tv.availableSizes.some((size) => size >= 65)));
-console.log(largeTvs);
-
-// opdracht 1f:
-const ambiLightTvs = inventory.filter((tv) => {
-    const ambiLightOption = tv.options.find((ambiLightOption) => ambiLightOption.name === "ambiLight");
-    return ambiLightOption.applicable === true;
-});
-console.log(ambiLightTvs);
-
